18a75a79764a0ac6d9e6655f9231bd5d35bf263a
andrewjethrovpena <andrewjethrovpena@gmail.com>
2020-01-13 22:58:18 +0800

    first commit

---
+++ database/migrations/2020_01_11_202221_create_expense_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpenseCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expense_categories', function (Blueprint $table) {
+            $table->bigIncrements('category_id');
+            $table->string('expense_name');
+            $table->string('expense_category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expense_categories');
+    }
+}
---
+++ database/migrations/2020_01_11_202237_create_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->bigIncrements('transaction_id');
+            $table->integer('category_id')->length(10)->unsigned();
+            $table->double('amount', 10, 2);
+            $table->dateTime('transaction_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
